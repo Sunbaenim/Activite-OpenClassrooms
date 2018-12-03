1028cb112bb355f71b0451964c999d1a434b36cf
Kaze <filleduvoisin@gmail.com>
2018-12-03 10:40:12 +0100

    Ajout de argv.c, version améliorée de putstr.c

---
+++ argv.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int i;
+	int j;
+
+	i = 1;
+	j = 0;
+	while (i < argc)
+	{
+		while (argv[i][j])
+		{
+			ft_putchar(argv[i][j]);
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+	return (0);
+}
